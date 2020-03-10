1482a6d3017948f4f12166528b600133e063b568
joshuaai <joshuaiheji@gmail.com>
2020-03-10 12:12:15 +0100

    set up workbox and cache homepage

---
+++ project/workbox-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    globDirectory: 'build/',
+    globPatterns: [
+        '**/*.css',
+        'index.html',
+        'js/animation.js',
+        'images/home/*.jpg',
+        'images/icon/*.svg'
+    ],
+    swDest: 'build/sw.js',
+    swSrc: 'src/sw.js',
+    globIgnores: ['../workbox-config.js']
+}
