aac03990eff0cb6e4062a3acfdebc56ebe85ab05
joshuaai <joshuaiheji@gmail.com>
2020-03-10 14:37:30 +0100

    implement the staleWhileRevalidate and network-first caching strategies

--- project/src/sw.js
+++ project/src/sw.js
@@ -18,10 +18,49 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
 )
 
+const articleHandler = workbox.strategies.networkFirst({
+    cacheName: 'articles-cache',
+    plugins: [
+        new workbox.expiration.Plugin({
+            maxEntries: 50
+        })
+    ]
+})
+
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`)
 
     workbox.precaching.precacheAndRoute([])
+
+    workbox.routing.registerRoute(
+        /(.*)articles(.*)\.(?:png|gif|jpg)/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+                })
+            ]
+        })
+    )
+
+    workbox.routing.registerRoute(
+        new RegExp('/images/icon'),
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'icon-cache',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 5,
+                    maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+                })
+            ]
+        })
+    )
+
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+        return articleHandler.handle(args)
+    })
 } else {
     console.log(`Boo! Workbox didn't load 😬`)
 }
